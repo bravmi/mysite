a7acf000593a9ef256f3c3fe565a23241c4196a7
Michael T <bravmech@gmail.com>
2020-05-22 11:32:35 +0500

    redirect to polls from root

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
+    path('', lambda request: redirect('polls/', permanent=False)),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
