07d40f0b7458bd4270a52874d39a640980221a11
Michael T <bravmech@gmail.com>
2020-06-06 15:32:05 +0500

    auto create profile for a user

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,8 @@ import datetime
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
 
 
@@ -39,6 +41,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_and_save_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 
 class Comment(models.Model):
