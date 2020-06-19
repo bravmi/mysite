e51e3b21558b3ae288344f1b6ef25eb633390eb6
Michael T <bravmech@gmail.com>
2020-06-19 11:25:41 +0500

    update url order

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,9 +4,9 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', lambda request: redirect('polls/', permanent=False)),
     path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
